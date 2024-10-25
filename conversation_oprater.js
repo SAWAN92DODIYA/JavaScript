dd570c29da0a9323dd31020d608b65a0b7e43c5d
developer <sawandodiya02@gmail.com>
2024-10-25 12:00:15 +0530

    Add Comparison , Conversation and Test file

---
+++ conversation_oprater.js
@@ -0,0 +1,21 @@
+console.log(1+1)
+console.log(1+2+"23")
+console.log("1"+2+23)
+console.log(true)
+console.log(+true)
+console.log(+"")
+console.log(false)
+console.log(typeof true)
+console.log(typeof false)
+console.log(typeof null) // this typeof return object 
+console.log(((234+ 2345)*8)%2)
+
+let numberFirst , numberSecond , numberThird ;
+numberFirst = numberSecond = numberThird = 12300;
+console.log(numberFirst)
+
+{
+  
+}
+
+
