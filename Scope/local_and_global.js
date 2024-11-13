26e9c09abba0fe8cfd4533e03e978df13fa02311
Sawan <sawandodiya02@gmail.com>
2024-11-14 00:00:26 +0530

    add Scope

---
+++ Scope/local_and_global.js
@@ -0,0 +1,13 @@
+var a = 10;
+let b = 20;
+const c = 30;
+if(true){
+  var a = 34;  // is not follow scope 
+  let b = 22;
+  const c = 33;
+  console.log(`${a} ${b} ${c}`);
+
+}
+
+
+console.log(`${a} ${b} ${c}`);
